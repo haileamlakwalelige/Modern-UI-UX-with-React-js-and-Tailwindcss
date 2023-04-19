c43aae8a941b9122aab7ca082ceefcaa97f98bef
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-04-19 20:45:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-black text-white">
+  <h1 className="text-white">Hello, kingo! Are you ready for your magic!</h1>
+    </div>
+  );
+}
+
+export default App;
